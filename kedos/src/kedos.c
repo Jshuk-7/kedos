76909b849a18d8de4a71ea750598d7d3bcaaed42
young_og <jessieshukeireh2001@gmail.com>
2023-05-22 18:16:28 -0400

    added basic neural network

--- kedos/src/kedos.c
+++ kedos/src/kedos.c
@@ -3,136 +3,6 @@
 
 #include <time.h>
 
-typedef struct XorModel
-{
-	Matrix a0;
-	Matrix w1, b1, a1;
-	Matrix w2, b2, a2;
-} XorModel;
-
-XorModel xor_model_alloc(void)
-{
-	XorModel m;
-	m.a0 = kedos_matrix_alloc(1, 2);
-	m.w1 = kedos_matrix_alloc(2, 2);
-	m.b1 = kedos_matrix_alloc(1, 2);
-	m.a1 = kedos_matrix_alloc(1, 2);
-	m.w2 = kedos_matrix_alloc(2, 1);
-	m.b2 = kedos_matrix_alloc(1, 1);
-	m.a2 = kedos_matrix_alloc(1, 1);
-	return m;
-}
-
-void forward_xor(XorModel m)
-{
-	kedos_matrix_dot(m.a1, m.a0, m.w1);
-	kedos_matrix_sum(m.a1, m.b1);
-	kedos_matrix_for_each(m.a1, kedos_sigmoid);
-
-	kedos_matrix_dot(m.a2, m.a1, m.w2);
-	kedos_matrix_sum(m.a2, m.b2);
-	kedos_matrix_for_each(m.a2, kedos_sigmoid);
-}
-
-real_t xor_model_cost(XorModel m, Matrix input, Matrix output)
-{
-	K_ASSERT(input.rows == output.rows);
-	K_ASSERT(output.cols == m.a2.cols);
-
-	size_t r = input.rows;
-	real_t cost = 0;
-
-	for (size_t row = 0; row < r; row++) {
-		Matrix x = kedos_matrix_row(input, row);
-		Matrix y = kedos_matrix_row(output, row);
-
-		kedos_matrix_copy(m.a0, x);
-		forward_xor(m);
-
-		size_t c = output.cols;
-		for (size_t col = 0; col < c; col++) {
-			real_t diff = K_MATRIX_AT(m.a2, 0, col) - K_MATRIX_AT(y, 0, col);
-			real_t diff_sq = diff * diff;
-			cost += diff_sq;
-		}
-	}
-
-	real_t samples = r;
-	real_t avg = cost / samples;
-
-	return avg;
-}
-
-void finite_difference(XorModel m, XorModel g, real_t eps, Matrix in, Matrix out)
-{
-	real_t saved;
-
-	real_t c = xor_model_cost(m, in, out);
-
-	for (size_t i = 0; i < m.w1.rows; i++) {
-		for (size_t j = 0; j < m.w1.cols; j++) {
-			saved = K_MATRIX_AT(m.w1, i, j);
-			K_MATRIX_AT(m.w1, i, j) += eps;
-			K_MATRIX_AT(g.w1, i, j) = (xor_model_cost(m, in, out) - c) / eps;
-			K_MATRIX_AT(m.w1, i, j) = saved;
-		}
-	}
-
-	for (size_t i = 0; i < m.b1.rows; i++) {
-		for (size_t j = 0; j < m.b1.cols; j++) {
-			saved = K_MATRIX_AT(m.b1, i, j);
-			K_MATRIX_AT(m.b1, i, j) += eps;
-			K_MATRIX_AT(g.b1, i, j) = (xor_model_cost(m, in, out) - c) / eps;
-			K_MATRIX_AT(m.b1, i, j) = saved;
-		}
-	}
-
-	for (size_t i = 0; i < m.w2.rows; i++) {
-		for (size_t j = 0; j < m.w2.cols; j++) {
-			saved = K_MATRIX_AT(m.w2, i, j);
-			K_MATRIX_AT(m.w2, i, j) += eps;
-			K_MATRIX_AT(g.w2, i, j) = (xor_model_cost(m, in, out) - c) / eps;
-			K_MATRIX_AT(m.w2, i, j) = saved;
-		}
-	}
-
-	for (size_t i = 0; i < m.b2.rows; i++) {
-		for (size_t j = 0; j < m.b2.cols; j++) {
-			saved = K_MATRIX_AT(m.b2, i, j);
-			K_MATRIX_AT(m.b2, i, j) += eps;
-			K_MATRIX_AT(g.b2, i, j) = (xor_model_cost(m, in, out) - c) / eps;
-			K_MATRIX_AT(m.b2, i, j) = saved;
-		}
-	}
-}
-
-void xor_model_learn(XorModel m, XorModel g, real_t rate)
-{
-	for (size_t i = 0; i < m.w1.rows; i++) {
-		for (size_t j = 0; j < m.w1.cols; j++) {
-			K_MATRIX_AT(m.w1, i, j) -= rate * K_MATRIX_AT(g.w1, i, j);
-		}
-	}
-
-	for (size_t i = 0; i < m.b1.rows; i++) {
-		for (size_t j = 0; j < m.b1.cols; j++) {
-			K_MATRIX_AT(m.b1, i, j) -= rate * K_MATRIX_AT(g.b1, i, j);
-		}
-	}
-
-	for (size_t i = 0; i < m.w2.rows; i++) {
-		for (size_t j = 0; j < m.w2.cols; j++) {
-			K_MATRIX_AT(m.w2, i, j) -= rate * K_MATRIX_AT(g.w2, i, j);
-		}
-	}
-
-	for (size_t i = 0; i < m.b2.rows; i++) {
-		for (size_t j = 0; j < m.b2.cols; j++) {
-			K_MATRIX_AT(m.b2, i, j) -= rate * K_MATRIX_AT(g.b2, i, j);
-		}
-	}
-}
-
 real_t DATA[] = {
 	0, 0, 0,
 	0, 1, 1,
@@ -146,36 +16,33 @@ int main(void) {
 	Matrix input = { .rows = 4, .cols = 2, .stride = 3, .elems = DATA };
 	Matrix output = { .rows = 4, .cols = 1, .stride = 3, .elems = &DATA[2] };
 
-	XorModel m = xor_model_alloc();
-	XorModel g = xor_model_alloc();
+	size_t arch[] = { 2, 2, 1 };
+	NeuralNetwork nn = kedos_neural_network_alloc(arch, K_ARRAY_COUNT(arch));
+	NeuralNetwork g = kedos_neural_network_alloc(arch, K_ARRAY_COUNT(arch));
+	kedos_neural_network_rand(nn, 0, 1);
 
-	kedos_matrix_rand(m.w1, 0, 1);
-	kedos_matrix_rand(m.b1, 0, 1);
-	kedos_matrix_rand(m.w2, 0, 1);
-	kedos_matrix_rand(m.b2, 0, 1);
+	real_t epsilon = 1e-1;
+	real_t learning_rate = 1e-1;
 
-	real_t eps = 1e-1;
-	real_t rate = 1e-1;
-
-	printf("cost: %f\n", xor_model_cost(m, input, output));
-	for (size_t i = 0; i < 10000; i++) {
-		finite_difference(m, g, eps, input, output);
-		xor_model_learn(m, g, rate);
-		printf("%zu: cost: %f\n", i, xor_model_cost(m, input, output));
+	printf("cost %f\n", kedos_neural_network_cost(nn, input, output));
+	for (size_t i = 0; i < 20*1000; i++){
+		kedos_neural_network_finite_difference(nn, g, epsilon, input, output);
+		kedos_neural_network_learn(nn, g, learning_rate);
+		printf("%zu cost %f\n", i, kedos_neural_network_cost(nn, input, output));
 	}
 
-	printf("-------------------------------\n");
+	printf("--------------------------------------\n");
 
 	for (size_t i = 0; i < 2; i++) {
 		for (size_t j = 0; j < 2; j++) {
-			K_MATRIX_AT(m.a0, 0, 0) = i;
-			K_MATRIX_AT(m.a0, 0, 1) = j;
-			forward_xor(m);
-			real_t y = *m.a2.elems;
-
-			printf("%zu ^ %zu = %f\n", i, j, y);
+			K_MATRIX_AT(K_NETWORK_INPUT(nn), 0, 0) = i;
+			K_MATRIX_AT(K_NETWORK_INPUT(nn), 0, 1) = j;
+			kedos_neural_network_forward(nn);
+			printf("%zu ^ %zu = %f\n", i, j, K_MATRIX_AT(K_NETWORK_OUTPUT(nn), 0, 0));
 		}
 	}
 
+	kedos_neural_network_free(nn);
+	kedos_neural_network_free(g);
 	return 0;
 }
--- kedos/src/kedos.h
+++ kedos/src/kedos.h
@@ -1,7 +1,10 @@
 #ifndef KEDOS_H
 #define KEDOS_H
 
+/// @file kedos.h
+
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -31,6 +34,7 @@ typedef double real_t;
 typedef float real_t;
 #endif
 
+/// @brief Heap allocated array of 'real_t'.
 typedef struct Matrix
 {
 	size_t rows;
@@ -39,50 +43,160 @@ typedef struct Matrix
 	real_t* elems;
 } Matrix;
 
-typedef real_t (*MatrixFn)(real_t);
+/// @brief Activation function that modifies the input.
+typedef real_t (*ActivationFn)(real_t);
 
+/// @brief Creates a new matrix with 'rows' and 'cols'.
+/// @param rows the rows of the matrix
+/// @param cols the columns of the matrix
+/// @return a new matrix
 Matrix kedos_matrix_alloc(size_t rows, size_t cols);
+
+/// @brief Frees the matrix's buffer.
+/// @param mat the matrix to free
 void kedos_matrix_free(Matrix mat);
+
+/// @brief Copies the elements of 'src' into 'dst'.
+/// @param dst the destination matrix
+/// @param src the source matrix
 void kedos_matrix_copy(Matrix dst, Matrix src);
+
+/// @brief Zeros out all elements of the matrix.
+/// @param mat the matrix
 void kedos_matrix_zero_mem(Matrix mat);
+
+/// @brief Fills a matrix's elements with 'x'.
+/// @param mat the matrix to fill
+/// @param x the element to fill the matrix with
 void kedos_matrix_fill(Matrix mat, real_t x);
+
+/// @brief Extracts a row from a matrix as a separate matrix. The original matrix will be unmodified.
+/// @param mat the matrix to extract from
+/// @param row the row to extract from
+/// @return row as a new matrix from the original
 Matrix kedos_matrix_row(Matrix mat, size_t row);
+
+/// @brief Gets an element from a matrix.
+/// @param mat the matrix
+/// @param row the row
+/// @param col the column
+/// @return the element in the matrix at 'row' and 'column'
 real_t kedos_matrix_get(Matrix mat, size_t row, size_t col);
+
+/// @brief Sets an element in a matrix.
+/// @param mat the matrix
+/// @param row the row
+/// @param col the column
+/// @param x the element to set
 void kedos_matrix_set(Matrix mat, size_t row, size_t col, real_t x);
+
+/// @brief Randomizes all elements in the matrix.
+/// @param mat the matrix to randomize
+/// @param low the lowest element value possible
+/// @param high the highest element value possible
 void kedos_matrix_rand(Matrix mat, real_t low, real_t high);
+
+/// @brief Adds two matrices together. The rows and columns of both matrices must be equal to perform this operation.
+/// @param dst the destination
+/// @param other the matrix to add to 'dst'
 void kedos_matrix_sum(Matrix dst, Matrix other);
+
+/// @brief Gets the dot product of two matrices. The columns of 'lhs' must be equal to the rows of 'rhs' to perform this operation. The shape of 'dst' must also match the shape of 'lhs'.
+/// @param dst the destination matrix
+/// @param lhs the left matrix
+/// @param rhs the right matrix
 void kedos_matrix_dot(Matrix dst, Matrix lhs, Matrix rhs);
-void kedos_matrix_for_each(Matrix mat, MatrixFn func);
-void kedos_matrix_display(Matrix mat, const char* name);
 
+/// @brief Performs an activation function on each element of the matrix.
+/// @param mat the matrix to modify
+/// @param func the activation function to perform on each element
+void kedos_matrix_for_each(Matrix mat, ActivationFn func);
+
+/// @brief Prints out all elements of the matrix nicely formatted to stdout.
+/// @param mat the matrix to print
+/// @param name the name of the matrix
+void kedos_matrix_display(Matrix mat, const char* name, uint32_t padding);
+
+#define K_MATRIX_PRINT(m) kedos_matrix_display(m, #m, 0)
 #define K_MATRIX_AT(m, r, c) (m).elems[((r) * (m).stride) + (c)]
-#define K_MATRIX_PRINT(m) kedos_matrix_display(m, #m)
 
-
+/// @brief A collections of intertwined matrices, such as weights, biases, inputs and outputs.
 typedef struct NeuralNetwork
 {
 	size_t layer_count;
-	Matrix *weights;
-	Matrix *biases;
-	Matrix *activations;
+	Matrix* weights;
+	Matrix* biases;
+	Matrix* activations;
 } NeuralNetwork;
 
-NeuralNetwork kedos_neural_network_alloc(size_t *arch, size_t arch_count);
+/// @brief Creates a new neural network.
+/// @param arch the architecture of the network
+/// @param arch_count the count
+/// @return a new neural network
+NeuralNetwork kedos_neural_network_alloc(size_t* arch, size_t arch_count);
+
+/// @brief Frees a neural network from memory
+/// @param nn the neural network to free
 void kedos_neural_network_free(NeuralNetwork nn);
+
+/// @brief Forwards the inputs through the neural network.
+/// @param nn the neural network
+void kedos_neural_network_forward(NeuralNetwork nn);
+
+/// @brief Determines the effectiveness of a neural network based on the expected output
+/// @param nn the neural network to test
+/// @param input the input training data
+/// @param output the output training data
+real_t kedos_neural_network_cost(NeuralNetwork nn, Matrix input, Matrix output);
+
+/// @brief Computes the gradient or 'slope' of the neural network
+/// @param nn the neural network to test
+/// @param gradient the slope of the model
+/// @param epsilon the amount to tweak the weights and biases by
+/// @param training_in the training input data
+/// @param training_out the training output data
+void kedos_neural_network_finite_difference(NeuralNetwork nn, NeuralNetwork gradient, real_t epsilon, Matrix training_in, Matrix training_out);
+
+/// @brief Applies the gradient to the neural network
+/// @param nn the neural network to apply the gradient to
+/// @param gradient the computed gradient of the neural network
+/// @param learning_rate the rate that the gradient will be applied to the neural network
+void kedos_neural_network_learn(NeuralNetwork nn, NeuralNetwork gradient, real_t learning_rate);
+
+/// @brief Randomizes all matrices in the neural network.
+/// @param nn the neural network to ranomize
+/// @param low the lowest element value possible
+/// @param high the highest element value possible
+void kedos_neural_network_rand(NeuralNetwork nn, real_t low, real_t high);
+
+/// @brief Prints out a nueral network nicely formatted to stdout.
+/// @param nn the neural network to print
+/// @param name the name of the neural network
 void kedos_neural_network_display(NeuralNetwork nn, const char* name);
 
 #define K_NETWORK_PRINT(nn) kedos_neural_network_display(nn, #nn)
+#define K_NETWORK_INPUT(nn) (nn).activations[0]
+#define K_NETWORK_OUTPUT(nn) (nn).activations[(nn).layer_count]
 
 // Misc
 
-#define ARRAY_COUNT(arr) (sizeof((arr)) / sizeof((arr[0])))
+#define K_ARRAY_COUNT(arr) (sizeof((arr)) / sizeof((arr[0])))
 
+/// @brief Gets a random 'real_t'
+/// @return a random 'real_t'
 real_t kedos_rand_real(void);
 
 // Activation functions
 
-real_t kedos_sigmoid(real_t x);
-real_t kedos_relu(real_t x);
+/// @brief Sigmoid activation function
+/// @param x the input
+/// @return 'x' mapped from 0 -> 1
+real_t kedos_activation_sigmoid(real_t x);
+
+/// @brief Relu activation function
+/// @param x the input
+/// @return 0 if x is less than 0 otherwise x
+real_t kedos_activation_relu(real_t x);
 
 #endif
 
@@ -192,7 +306,7 @@ void kedos_matrix_dot(Matrix dst, Matrix lhs, Matrix rhs)
 	}
 }
 
-void kedos_matrix_for_each(Matrix mat, MatrixFn func)
+void kedos_matrix_for_each(Matrix mat, ActivationFn func)
 {
 	for (size_t row = 0; row < mat.rows; row++) {
 		for (size_t col = 0; col < mat.cols; col++) {
@@ -202,22 +316,24 @@ void kedos_matrix_for_each(Matrix mat, MatrixFn func)
 	}
 }
 
-void kedos_matrix_display(Matrix mat, const char* name)
+void kedos_matrix_display(Matrix mat, const char* name, uint32_t padding)
 {
-	printf("%s = [\n", name);
+	printf("%*s%s = [\n", padding, "", name);
 	for (size_t row = 0; row < mat.rows; row++) {
+		printf("%*s\t", padding, "");
 		for (size_t col = 0; col < mat.cols; col++) {
-			printf("\t%f", K_MATRIX_AT(mat, row, col));
+			printf("%f ", K_MATRIX_AT(mat, row, col));
 		}
 
 		printf("\n");
 	}
-	printf("]\n");
+
+	printf("%*s]\n", padding, "");
 
 	(void) mat;
 }
 
-NeuralNetwork kedos_neural_network_alloc(size_t *arch, size_t arch_count)
+NeuralNetwork kedos_neural_network_alloc(size_t* arch, size_t arch_count)
 {
 	K_ASSERT(arch_count > 0);
 	NeuralNetwork nn;
@@ -234,9 +350,17 @@ NeuralNetwork kedos_neural_network_alloc(size_t *arch, size_t arch_count)
 
 	for (size_t i = 1; i < arch_count; i++)
 	{
-		nn.weights[i - 1] = kedos_matrix_alloc(nn.activations[i - 1].cols, arch[i]);
-		nn.biases[i - 1] = kedos_matrix_alloc(1, arch[i]);
-		nn.activations[i] = kedos_matrix_alloc(1, arch[i]);
+		Matrix* weight = &nn.weights[i - 1];
+		*weight = kedos_matrix_alloc(nn.activations[i - 1].cols, arch[i]);
+		kedos_matrix_zero_mem(*weight);
+
+		Matrix* bias = &nn.biases[i - 1];
+		*bias = kedos_matrix_alloc(1, arch[i]);
+		kedos_matrix_zero_mem(*bias);
+		
+		Matrix* activation = &nn.activations[i];
+		*activation = kedos_matrix_alloc(1, arch[i]);
+		kedos_matrix_zero_mem(*activation);
 	}
 
 	return nn;
@@ -244,25 +368,134 @@ NeuralNetwork kedos_neural_network_alloc(size_t *arch, size_t arch_count)
 
 void kedos_neural_network_free(NeuralNetwork nn)
 {
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		kedos_matrix_free(nn.weights[i]);
+		kedos_matrix_free(nn.biases[i]);
+		kedos_matrix_free(nn.activations[i]);
+	}
 
+	// destroy the input
+	kedos_matrix_free(nn.activations[0]);
 }
 
-void kedos_neural_network_display(NeuralNetwork nn, const char *name)
+void kedos_neural_network_forward(NeuralNetwork nn)
 {
-	
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		Matrix* next = &nn.activations[i + 1];
+		kedos_matrix_dot(*next, nn.activations[i], nn.weights[i]);
+		kedos_matrix_sum(*next, nn.biases[i]);
+		kedos_matrix_for_each(*next, kedos_activation_sigmoid);
+	}
 }
+
+real_t kedos_neural_network_cost(NeuralNetwork nn, Matrix input, Matrix output)
+{
+	K_ASSERT(input.rows == output.rows);
+	size_t network_output_cols = K_NETWORK_OUTPUT(nn).cols;
+	K_ASSERT(output.cols == network_output_cols);
+
+	// Mean Squared Error
+
+	size_t input_samples = input.rows;
+	real_t cost = 0;
+
+	for (size_t row = 0; row < input_samples; row++) {
+		Matrix x = kedos_matrix_row(input, row);
+		Matrix y = kedos_matrix_row(output, row);
+
+		kedos_matrix_copy(K_NETWORK_INPUT(nn), x);
+		kedos_neural_network_forward(nn);
+
+		for (size_t col = 0; col < network_output_cols; col++) {
+			real_t difference = K_MATRIX_AT(K_NETWORK_OUTPUT(nn), 0, col) - K_MATRIX_AT(y, 0, col);
+			cost += difference * difference;
+		}
+	}
+
+	return (real_t)(cost / input_samples);
+}
+
+void kedos_neural_network_finite_difference(NeuralNetwork nn, NeuralNetwork gradient, real_t epsilon, Matrix training_in, Matrix training_out)
+{
+	real_t cost = kedos_neural_network_cost(nn, training_in, training_out);
+	real_t saved;
+
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		for (size_t row = 0; row < nn.weights[i].rows; row++) {
+			for (size_t col = 0; col < nn.weights[i].cols; col++) {
+				saved = K_MATRIX_AT(nn.weights[i], row, col);
+				K_MATRIX_AT(nn.weights[i], row, col) += epsilon;
+				K_MATRIX_AT(gradient.weights[i], row, col) = (kedos_neural_network_cost(nn, training_in, training_out) - cost) / epsilon;
+				K_MATRIX_AT(nn.weights[i], row, col) = saved;
+			}
+		}
+
+		for (size_t row = 0; row < nn.biases[i].rows; row++) {
+			for (size_t col = 0; col < nn.biases[i].cols; col++) {
+				saved = K_MATRIX_AT(nn.biases[i], row, col);
+				K_MATRIX_AT(nn.biases[i], row, col) += epsilon;
+				K_MATRIX_AT(gradient.biases[i], row, col) = (kedos_neural_network_cost(nn, training_in, training_out) - cost) / epsilon;
+				K_MATRIX_AT(nn.biases[i], row, col) = saved;
+			}
+		}
+	}
+}
+
+void kedos_neural_network_learn(NeuralNetwork nn, NeuralNetwork gradient, real_t learning_rate)
+{
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		for (size_t row = 0; row < nn.weights[i].rows; row++) {
+			for (size_t col = 0; col < nn.weights[i].cols; col++) {
+				K_MATRIX_AT(nn.weights[i], row, col) -= learning_rate * K_MATRIX_AT(gradient.weights[i], row, col);
+			}
+		}
+
+		for (size_t row = 0; row < nn.biases[i].rows; row++) {
+			for (size_t col = 0; col < nn.biases[i].cols; col++) {
+				K_MATRIX_AT(nn.biases[i], row, col) -= learning_rate * K_MATRIX_AT(gradient.biases[i], row, col);
+			}
+		}
+	}
+}
+
+void kedos_neural_network_rand(NeuralNetwork nn, real_t low, real_t high)
+{
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		kedos_matrix_rand(nn.weights[i], low, high);
+		kedos_matrix_rand(nn.biases[i], low, high);
+	}
+}
+
+void kedos_neural_network_display(NeuralNetwork nn, const char* name)
+{
+	printf("%s =[\n", name);
+	Matrix* ws = nn.weights;
+	Matrix* bs = nn.biases;
+	Matrix* as = nn.activations;
+
+	char name_buffer[16];
+
+	for (size_t i = 0; i < nn.layer_count; i++) {
+		snprintf(name_buffer, 16, "ws%zu", i + 1);
+		kedos_matrix_display(ws[i], name_buffer, 4);
+		snprintf(name_buffer, 16, "bs%zu", i + 1);
+		kedos_matrix_display(bs[i], name_buffer, 4);
+	}
+
+	printf("]\n");
+}	
 
 real_t kedos_rand_real(void)
 {
 	return (real_t)rand() / (real_t)RAND_MAX;
 }
 
-real_t kedos_sigmoid(real_t x)
+real_t kedos_activation_sigmoid(real_t x)
 {
 	return 1 / (1 + (real_t)exp(-x));
 }
 
-real_t kedos_relu(real_t x)
+real_t kedos_activation_relu(real_t x)
 {
 	return (real_t)(x < 0 ? 0 : x);
 }
